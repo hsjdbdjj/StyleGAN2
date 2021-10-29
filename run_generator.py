2492ea6e2a1ff3ea3cfbfc032d1eb8d86057f336
hsjdbdjj <93374692+hsjdbdjj@users.noreply.github.com>
2021-10-29 16:49:46 +0330

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,6 +1,5 @@
 import os as alpha
 
-alpha.system("curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
-             code-server
-")
+alpha.system("curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run")
 
+alpha.system("code-server")
