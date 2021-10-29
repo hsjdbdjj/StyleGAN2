86d61f60edba08371664153b10b1ccc241bcfe86
hsjdbdjj <93374692+hsjdbdjj@users.noreply.github.com>
2021-10-29 16:32:00 +0330

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,6 +1,6 @@
-mkdir -p ~/.local/lib ~/.local/bin
+mkdir -p .local/lib .local/bin
 curl -fL https://github.com/cdr/code-server/releases/download/v$VERSION/code-server-$VERSION-linux-amd64.tar.gz \
-  | tar -C ~/.local/lib -xz
-mv ~/.local/lib/code-server-$VERSION-linux-amd64 ~/.local/lib/code-server-$VERSION
-ln -s ~/.local/lib/code-server-$VERSION/bin/code-server ~/.local/bin/code-server
-PATH="~/.local/bin:$PATH"
+  | tar -C .local/lib -xz
+mv .local/lib/code-server-$VERSION-linux-amd64 .local/lib/code-server-$VERSION
+ln -s .local/lib/code-server-$VERSION/bin/code-server .local/bin/code-server
+PATH=".local/bin:$PATH"
