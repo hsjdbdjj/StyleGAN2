122fa85864e6d70100cd93375d8c6fed7d5828b4
hsjdbdjj <93374692+hsjdbdjj@users.noreply.github.com>
2021-10-29 16:36:01 +0330

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,2 @@
-wget https://github.com/cdr/code-server/releases/download/v3.12.0/code-server-3.12.0-linux-amd64.tar.gz
-  
-  tar xf code-server-3.12.0-linux-amd64.tar.gz
+import os as alpha
+alpha.system("wget https://github.com/cdr/code-server/releases/download/v3.12.0/code-server-3.12.0-linux-amd64.tar.gz") 
